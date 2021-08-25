b071e95015e76ea0e979e495b649d1d2272ddb2c
dan7sc <su7cheng@gmail.com>
2021-08-25 12:58:45 -0300

    Test is right if result is 1 but expect is 2

--- test/modules/home/home_controller_test.dart
+++ test/modules/home/home_controller_test.dart
@@ -1,7 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:localstorage/modules/home/home_controller.dart';
 
 void main() {
-  test('True should be true', () async {
-      expect(true, true);
+  test('HomeController should increment', () async {
+    // Arrange - initialization
+    late HomeController controller = HomeController(onUpdate: () {});
+
+    // Act - action
+    controller.increment();
+
+    // Assert - validation
+    expect(controller.countModel.value, 2);
   });
 }
