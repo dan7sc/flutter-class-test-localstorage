b92f7f6011b3030c86977fd34b3a5388ddff2439
dan7sc <su7cheng@gmail.com>
2021-08-25 13:08:38 -0300

    Test failed for result 3 expecting 2

--- test/modules/home/home_controller_test.dart
+++ test/modules/home/home_controller_test.dart
@@ -2,14 +2,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:localstorage/modules/home/home_controller.dart';
 
 void main() {
-  test('HomeController should increment', () async {
-    // Arrange - initialization
-    late HomeController controller = HomeController(onUpdate: () {});
+  group('HomeController increment', () {
+    test('Should increment', () async {
+      // Arrange - initialization
+      late HomeController controller = HomeController(onUpdate: () {});
 
-    // Act - action
-    controller.increment();
+      // Act - action
+      controller.increment();
 
-    // Assert - validation
-    expect(controller.countModel.value, 1);
+      // Assert - validation
+      expect(controller.countModel.value, 1);
+    });
+
+    test('Should increment multiple times', () async {
+      // Arrange - initialization
+      late HomeController controller = HomeController(onUpdate: () {});
+
+      // Act - action
+      controller.increment();
+      controller.increment();
+      controller.increment();
+
+      // Assert - validation
+      expect(controller.countModel.value, 2);
+    });
   });
 }
